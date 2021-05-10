243f3c9dc263c2edc93f21a222c0f3d33d5ae586
Juliano Ventola <ventolajuliano@gmail.com>
2021-05-10 02:36:08 -0300

    feat: xylophone playing notes

--- lib/pages/xylophone_page.dart
+++ lib/pages/xylophone_page.dart
@@ -1,31 +1,45 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
-class XylophonePage extends StatefulWidget {
+class XylophonePage extends StatelessWidget {
   XylophonePage({Key? key}) : super(key: key);
 
-  @override
-  _XylophonePageState createState() => _XylophonePageState();
-}
-
-class _XylophonePageState extends State<XylophonePage> {
-  final AudioCache audioPlayer = AudioCache();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            playLocal();
-          },
-          child: Text('Play'),
+  Expanded _buildSoundKey({required int index, required Color color}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          final AudioCache audioPlayer = AudioCache();
+          audioPlayer.play('note$index.wav');
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(color),
+        ),
+        child: Container(
+          width: 150,
+          height: 30,
         ),
       ),
     );
   }
 
-  playLocal() {
-    audioPlayer.play('note5.wav');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _buildSoundKey(index: 1, color: Colors.red),
+            _buildSoundKey(index: 2, color: Colors.orange),
+            _buildSoundKey(index: 3, color: Colors.yellow),
+            _buildSoundKey(index: 4, color: Colors.green),
+            _buildSoundKey(index: 5, color: Colors.teal),
+            _buildSoundKey(index: 6, color: Colors.blue),
+            _buildSoundKey(index: 7, color: Colors.purple),
+          ],
+        ),
+      ),
+    );
   }
 }
